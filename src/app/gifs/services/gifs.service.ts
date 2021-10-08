4e1325d3d36cf0274d1f6adcab4d661a849befe1
Rolando <rolandoguerra1991@gmail.com>
2021-10-08 16:57:29 -0400

    tipado de respuesta

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private APIKEY:string = "gwYmNzCsQm0Yct0CzJ1umliYT3pp6lo6";
   private _historial:string[] = [];
+  public resultados:Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http:HttpClient) { }
 
   buscarGifs(query:string = '') {
     query = query.trim().toLocaleLowerCase();
@@ -19,9 +24,11 @@ export class GifsService {
       this._historial = this._historial.slice(0, 10);
     }
 
-
-    console.log(this._historial);
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=gwYmNzCsQm0Yct0CzJ1umliYT3pp6lo6&q=${query}`)
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
   }
 
-  constructor() { }
 }
